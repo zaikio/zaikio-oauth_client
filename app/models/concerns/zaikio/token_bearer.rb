c7ff0ece685315dbffd497b715dc46d22b66d3f1
Steffen Boller <mail@steffen-boller.de>
2020-02-05 14:48:53 +0100

    add last_valid_or_expired_token

--- app/models/concerns/zaikio/token_bearer.rb
+++ app/models/concerns/zaikio/token_bearer.rb
@@ -5,4 +5,8 @@ module Zaikio::TokenBearer
     # Associations
     has_many :access_tokens, as: :bearer, class_name: 'Zaikio::AccessToken'
   end
+
+  def last_valid_or_expired_token
+    access_tokens.valid&.first&.token || access_tokens.last.token
+  end
 end
--- test/dummy/app/controllers/current_person_controller.rb
+++ test/dummy/app/controllers/current_person_controller.rb
@@ -1,6 +1,6 @@
 class CurrentPersonController < ApplicationController
   def show
-    directory = Zaikio.directory(token: Current.user.access_tokens.valid.first.token)
+    directory = Zaikio.directory(token: Current.user.last_valid_or_expired_token)
     remote_person = directory.person
 
     render plain: remote_person.attributes.to_json
