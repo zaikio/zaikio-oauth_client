a2f325e37a731acfd5147128b6cdc2e2cb4f7f81
Steffen Boller <mail@steffen-boller.de>
2020-02-05 15:02:50 +0100

    remove dummy app migrations

--- test/dummy/db/migrate/20191121092907_enable_postgres_extensions_for_uuids.zaikio.rb
+++
@@ -1,6 +0,0 @@
-# This migration comes from zaikio (originally 20190426155505)
-class EnablePostgresExtensionsForUuids < ActiveRecord::Migration[6.0]
-  def change
-    enable_extension 'pgcrypto' unless extension_enabled?('pgcrypto')
-  end
-end
