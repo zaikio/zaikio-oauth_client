4704c5ff2f3f62a77aa44f10b40917010c150757
Christian Weyer <cw@crispymtn.com>
2020-02-05 21:20:47 +0100

    Fix incorrect string replacement in migration

--- db/migrate/20191031203820_create_zaikio_sites.rb
+++ db/migrate/20191031203820_create_zaikio_sites.rb
@@ -1,4 +1,4 @@
-class CreateioSites < ActiveRecord::Migration[6.0]
+class CreateZaikioSites < ActiveRecord::Migration[6.0]
   def change
     create_table :zaikio_sites, id: :uuid do |t|
       t.references :organization, null: false, index: true, type: :uuid, foreign_key: { to_table: :zaikio_organizations }
