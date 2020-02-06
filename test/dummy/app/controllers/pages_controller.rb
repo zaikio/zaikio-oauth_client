cc3199d29593a210941b87a1a127aa07c14bf35d
Matthias Prinz <mp@crispymtn.com>
2020-02-06 10:25:31 +0100

    General cleanups for layouts

--- test/dummy/app/controllers/pages_controller.rb
+++ test/dummy/app/controllers/pages_controller.rb
@@ -1,6 +1,15 @@
 class PagesController < ApplicationController
+  
   def show
-    @page = params[:page] || 'index'
+    add_breadcrumb I18n.t("breadcrumbs.#{params[:scope]}.overview"), "/#{params[:scope]}"
+
+    if params[:page]
+      @page = params[:page]
+      add_breadcrumb I18n.t("breadcrumbs.#{params[:scope]}.#{@page}"), "/#{params[:scope]}/#{@page}"
+    else
+      @page = 'index'
+    end
+
     render template: "pages/#{params[:scope]}/#{@page}"
   end
 end
