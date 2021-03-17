e0df0ed521bf723862cf7f05ab4e10ef5a81d63f
Nick Campbell <nc@zaikio.com>
2021-03-17 13:41:29 +0000

    Only lookup Hub::Models module under Zaikio::
    `const_defined?` takes two arguments. Given just the following code:
    
      module Zaikio; end
      module Webhooks; end
    
      Zaikio.const_defined?("Webhooks") => true # wrong
      const_defined?("Webhooks")        => true # correct
    
    We only want to find any Webhooks module _inside_ the Zaikio namespace,
    not the top-level one. We need to pass a second argument, `false`, to
    tell it to not recurse upwards until it finds something.

--- app/models/zaikio/access_token.rb
+++ app/models/zaikio/access_token.rb
@@ -62,7 +62,7 @@ module Zaikio
     end
 
     def bearer_klass
-      return unless Zaikio.const_defined?("Hub::Models")
+      return unless Zaikio.const_defined?("Hub::Models", false)
 
       if Zaikio::Hub::Models.configuration.respond_to?(:"#{bearer_type.underscore}_class_name")
         Zaikio::Hub::Models.configuration.public_send(:"#{bearer_type.underscore}_class_name").constantize
