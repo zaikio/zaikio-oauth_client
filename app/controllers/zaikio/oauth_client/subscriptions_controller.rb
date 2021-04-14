f7552dc7c3c9b5eead1895d6fdcef5660d0b3078
Nick Campbell <nc@zaikio.com>
2021-04-14 11:38:42 +0100

    Support :organization_id parameter with subscription flow
    This is useful when you know the Organization you wish to connect. Docs:
    https://docs.zaikio.com/api/directory/guides/subscriptions/redirect-flow/

--- app/controllers/zaikio/oauth_client/subscriptions_controller.rb
+++ app/controllers/zaikio/oauth_client/subscriptions_controller.rb
@@ -2,15 +2,21 @@ module Zaikio
   module OAuthClient
     class SubscriptionsController < ConnectionsController
       def new
-        opts = params.permit(:client_name, :state, :plan)
-        client_name = opts.delete(:client_name)
-        plan = opts.delete(:plan)
+        opts = params.permit(:client_name, :state, :plan, :organization_id)
 
-        subscription_scope = "Org.subscription_create"
+        plan            = opts.delete(:plan)
+        organization_id = opts.delete(:organization_id)
+
+        subscription_scope = if organization_id.present?
+                               "Org/#{organization_id}.subscription_create"
+                             else
+                               "Org.subscription_create"
+                             end
+
         subscription_scope << ".#{plan}" if plan.present?
 
         redirect_to oauth_client.auth_code.authorize_url(
-          redirect_uri: approve_url(client_name),
+          redirect_uri: approve_url(opts.delete(:client_name)),
           scope: subscription_scope,
           **opts
         )
