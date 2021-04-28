7566dffd1662883e675782930b7f25cd7f5c496a
Nick Campbell <nc@zaikio.com>
2021-04-28 13:28:11 +0100

    Use Rails reloader when configuring the OAuth client

--- test/dummy/config/initializers/zaikio_oauth_client.rb
+++ test/dummy/config/initializers/zaikio_oauth_client.rb
@@ -1,14 +1,16 @@
-Zaikio::OAuthClient.configure do |config|
-  config.environment = :test
-  config.logger = Rails.logger
-  config.sessions_controller_name = "sessions"
-  config.register_client :warehouse do |warehouse|
-    warehouse.client_id = "da6333fc-19ab-51d7-8295-4904358c5ecb"
-    warehouse.client_secret = "e415a98b72f0b48f554de75756f31780"
-    warehouse.default_scopes = %w[directory.person.r]
+Rails.application.reloader.to_prepare do
+  Zaikio::OAuthClient.configure do |config|
+    config.environment = :test
+    config.logger = Rails.logger
+    config.sessions_controller_name = "sessions"
+    config.register_client :warehouse do |warehouse|
+      warehouse.client_id = "da6333fc-19ab-51d7-8295-4904358c5ecb"
+      warehouse.client_secret = "e415a98b72f0b48f554de75756f31780"
+      warehouse.default_scopes = %w[directory.person.r]
 
-    warehouse.register_organization_connection do |org|
-      org.default_scopes = %w[directory.organization.r]
+      warehouse.register_organization_connection do |org|
+        org.default_scopes = %w[directory.organization.r]
+      end
     end
   end
 end
