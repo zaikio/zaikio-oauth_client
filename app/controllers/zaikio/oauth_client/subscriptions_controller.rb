ce76d2be671fcb2cf5b943e79e6815cf59548997
Polina <polina.toshkova@zaikio.com>
2022-10-05 20:31:25 +0300

    Allow subscription app name to be passed to Zaikio::OAuthClient::SubscriptionsController#new

--- app/controllers/zaikio/oauth_client/subscriptions_controller.rb
+++ app/controllers/zaikio/oauth_client/subscriptions_controller.rb
@@ -2,15 +2,19 @@ module Zaikio
   module OAuthClient
     class SubscriptionsController < ConnectionsController
       def new
-        opts = params.permit(:client_name, :state, :plan, :organization_id)
+        opts = params.permit(:client_name, :state, :plan, :organization_id, :app_name)
         opts[:state] ||= session[:state] = SecureRandom.urlsafe_base64(32)
 
         plan            = opts.delete(:plan)
         organization_id = opts.delete(:organization_id)
 
         subscription_scope = if organization_id.present?
+                               return "Org/#{organization_id}.subscription_create_#{app_name}" if app_name.present?
+
                                "Org/#{organization_id}.subscription_create"
                              else
+                               return "Org.subscription_create_#{app_name}" if app_name.present?
+
                                "Org.subscription_create"
                              end
 
